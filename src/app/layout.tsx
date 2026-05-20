4753a1c0be51ce7256ffaa2e366dcf32eadea2c5
hariando7 <rianaja2200022@gmail.com>
2026-05-20 07:56:03 +0700

    feat: bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 // import localFont from "next/font/local";
-// @ts-expect-error: side-effect import for global CSS (no type declarations)
 import "./globals.css";
 
 // const geistSans = localFont({
